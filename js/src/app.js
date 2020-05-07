b9ad3abb96d9ac78154e43e993cd5b254a3ebf46
Ignacio <irodrigues@makingsense.com>
2020-05-07 13:46:17 -0300

    update files js

--- js/src/app.js
+++ js/src/app.js
@@ -1,12 +1,1 @@
-/*Js file imports*/
-import $ from 'jquery';
-import transformicons from './vendors/transformicons';
-transformicons.add('.tcon');
-
-import slick from './vendors/slick.js';
-import easyAutocomplete from './vendors/jquery.easy-autocomplete.min.js';
-import './main.js';
-import sharer from "sharer.js";
-
-/*Sass file imports*/
-import  '../../css/src/style.scss';
+console.log("Okey");
--- js/src/main.js
+++
@@ -1,4 +0,0 @@
-import ScrollReveal from "scrollreveal";
-import moment from "moment";
-import easyAutocomplete from './vendors/jquery.easy-autocomplete.min.js';
-require('moment-timezone');
